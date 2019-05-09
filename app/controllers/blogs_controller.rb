107ba2fa85eedce7b660b57af398dabd6dcd3938
kyotarooooo <i.kentaro-0916@outlook.com>
2019-05-09 13:33:38 +0900

    [finish]3章投稿機能を作ろう

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+
   def show
   end
 
@@ -6,8 +7,21 @@ class BlogsController < ApplicationController
   end
 
   def new
+      @blog = Blog.new
+  end
+
+  def create
+  	  blog = Blog.new(blog_params)
+  	  blog.save
+  	  redirect_to blogs_path
+  end
+
+  private
+  def blog_params
+  	params.require(:blog).permit(:title, :category, :body)
   end
 
   def edit
   end
+
 end
